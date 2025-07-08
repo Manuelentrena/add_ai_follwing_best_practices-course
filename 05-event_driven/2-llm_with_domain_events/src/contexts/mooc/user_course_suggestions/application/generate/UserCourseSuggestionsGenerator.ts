ccdf7c8afa64bc8c78d89c67e177c886e9163237
entrenadev <manuel.entrena@gmail.com>
2025-07-08 13:09:57 +0200

    Refactor UserCourseSuggestionsGenerator to improve code formatting and readability

--- 05-event_driven/2-llm_with_domain_events/src/contexts/mooc/user_course_suggestions/application/generate/UserCourseSuggestionsGenerator.ts
+++ 05-event_driven/2-llm_with_domain_events/src/contexts/mooc/user_course_suggestions/application/generate/UserCourseSuggestionsGenerator.ts
@@ -5,25 +5,34 @@ import { UserCourseSuggestions } from "../../domain/UserCourseSuggestions";
 import { UserCourseSuggestionsRepository } from "../../domain/UserCourseSuggestionsRepository";
 
 export class UserCourseSuggestionsGenerator {
-	constructor(
-		private readonly repository: UserCourseSuggestionsRepository,
-		private readonly generator: CourseSuggestionsGenerator,
-		private readonly eventBus: EventBus,
-	) {}
+  constructor(
+    private readonly repository: UserCourseSuggestionsRepository,
+    private readonly generator: CourseSuggestionsGenerator,
+    private readonly eventBus: EventBus
+  ) {}
 
-	async generate(userId: string, courseName: string): Promise<void> {
-		const userCourseSuggestions =
-			(await this.repository.search(new UserId(userId))) ?? UserCourseSuggestions.create(userId);
+  async generate(userId: string, courseName: string): Promise<void> {
+    const userCourseSuggestions =
+      (await this.repository.search(new UserId(userId))) ??
+      UserCourseSuggestions.create(userId);
 
-		if (!userCourseSuggestions.hasCompleted(courseName)) {
-			userCourseSuggestions.addCompletedCourse(courseName);
+    if (!userCourseSuggestions.hasCompleted(courseName)) {
+      userCourseSuggestions.addCompletedCourse(courseName);
 
-			const suggestions = await this.generator.generate(userCourseSuggestions);
+      if (!this.generator.generateWithReason) {
+        throw new Error(
+          "generateWithReason is not implemented in the generator"
+        );
+      }
 
-			userCourseSuggestions.updateSuggestions(suggestions);
+      const suggestions = await this.generator.generateWithReason(
+        userCourseSuggestions
+      );
 
-			await this.repository.save(userCourseSuggestions);
-			await this.eventBus.publish(userCourseSuggestions.pullDomainEvents());
-		}
-	}
+      userCourseSuggestions.updateSuggestions(suggestions);
+
+      await this.repository.save(userCourseSuggestions);
+      await this.eventBus.publish(userCourseSuggestions.pullDomainEvents());
+    }
+  }
 }
