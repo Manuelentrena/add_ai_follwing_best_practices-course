14de613314faceaa13b4ddf2e14cb477a57a1e9c
entrenadev <manuel.entrena@gmail.com>
2025-07-08 13:09:41 +0200

    Update CourseSuggestionsGenerator interface to make methods optional

--- 05-event_driven/2-llm_with_domain_events/src/contexts/mooc/user_course_suggestions/domain/CourseSuggestionsGenerator.ts
+++ 05-event_driven/2-llm_with_domain_events/src/contexts/mooc/user_course_suggestions/domain/CourseSuggestionsGenerator.ts
@@ -1,5 +1,9 @@
+import { CourseSuggestion } from "./CourseSuggestion";
 import { UserCourseSuggestions } from "./UserCourseSuggestions";
 
 export interface CourseSuggestionsGenerator {
-	generate(userCourseSuggestions: UserCourseSuggestions): Promise<string>;
+  generate?(userCourseSuggestions: UserCourseSuggestions): Promise<string>;
+  generateWithReason?(
+    userCourseSuggestions: UserCourseSuggestions
+  ): Promise<CourseSuggestion[]>;
 }
